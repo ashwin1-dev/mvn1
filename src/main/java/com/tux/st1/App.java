fe7841ba775d1e56f9c0d1c66b07fe42f3753257
ashwin1-dev <69039026+ashwin1-dev@users.noreply.github.com>
2022-05-15 11:56:40 +0530

    Update App.java

--- src/main/java/com/tux/st1/App.java
+++ src/main/java/com/tux/st1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "On d footstep of BABA and Budhha" );
+        System.out.println( "On d footstep of BABA and Budhha... yes" );
     }
 }
