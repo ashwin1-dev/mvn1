c4a0db7cbf10accafdbcbd4cb0e9d58371cb2672
ashwin1-dev <ashwin.devop12@gmail.com>
2022-05-15 05:03:00 +0000

    mvn1 for jar file

---
+++ src/main/java/com/tux/st1/App.java
@@ -0,0 +1,13 @@
+package com.tux.st1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "On d footstep of BABA and Budhha" );
+    }
+}
